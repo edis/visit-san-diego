f12fcfc603f2f1b471b78c1d1b43844d2d91520f
edis <edis@gonuler.com>
2020-03-03 18:09:47 -0500

    add attractions text transition

--- src/js/main.js
+++ src/js/main.js
@@ -61,3 +61,29 @@ function init() {
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
 }
+
+
+const items = document.querySelectorAll('#attractions-title ');
+
+const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+const run = () =>
+    items.forEach(item => {
+        if (isInViewport(item)) {
+            item.classList.add('show');
+        }
+    });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
